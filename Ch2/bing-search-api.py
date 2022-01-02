dcf1a99357f837db739f72380ce18270901ef0e9
Achilleas Voutsas <avoutsas@gmail.com>
2022-01-02 14:15:18 +0000

    Updated Bing Search Spell Check API endpoint and key

--- Ch2/bing-search-api.py
+++ Ch2/bing-search-api.py
@@ -5,7 +5,7 @@ import json
 #%%
 api_key = '7d1be776156e45d7b08ca4a9cbe645c0'
 example_text = "Hollo, wrld" # the text to be spell-checked
-endpoint = "https://api.cognitive.microsoft.com/bing/v7.0/SpellCheck"
+endpoint = "https://api.bing.microsoft.com/v7.0/spellcheck"
 
 data = {'text': example_text}
 params = {
